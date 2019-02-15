63ec36e7fc91f6254fa20729a7520cc2c30ec5a9
Daniel Dyrnes <thedanielforum@gmail.com>
2019-02-15 17:31:50 +0800

    Updates

---
+++ precache-manifest.6f0b29bae1bd03ecdf7b3b22b3a1afe0.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "5c8a75a0d20baccbaa3e",
+    "url": "/shibagram/static/css/main.ea065c66.chunk.css"
+  },
+  {
+    "revision": "5c8a75a0d20baccbaa3e",
+    "url": "/shibagram/static/js/main.5c8a75a0.chunk.js"
+  },
+  {
+    "revision": "f301d01b74180653d3cb",
+    "url": "/shibagram/static/js/runtime~main.f301d01b.js"
+  },
+  {
+    "revision": "2f136cf89cec41ac83a3",
+    "url": "/shibagram/static/css/2.d2026522.chunk.css"
+  },
+  {
+    "revision": "2f136cf89cec41ac83a3",
+    "url": "/shibagram/static/js/2.2f136cf8.chunk.js"
+  },
+  {
+    "revision": "13086332cbbd98fc11e84605b804caf3",
+    "url": "/shibagram/index.html"
+  }
+];
